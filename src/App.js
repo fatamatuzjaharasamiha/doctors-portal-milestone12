f30efaa64a6f01cb7e5f6c6e62f5c946f05e3bcd
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2022-05-11 05:21:47 +0600

    final project start

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Shared/Login';
+import Navbar from './Pages/Shared/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
